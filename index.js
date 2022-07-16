27f031e564acb4afac71b21c0a43079ab96f2e5f
Jason <el.jsteer@gmail.com>
2022-07-16 15:57:12 +1000

    revamped content as per criteria

--- index.js
+++ index.js
@@ -11,30 +11,51 @@ function readMeDetails() {
     type: "input",
     name: "title",
     message: "What is the name of your Project or Repository?", 
+    validate: validateInput,
   }, {
-    type: "checkbox",
-    name: "Headings1",
-    message: "What optional headings would you like to include?",
-    choices: ["REQUIREMENTS", "INSTALLATION", "FAQ"]
+    type: "input",
+    name: "description",
+    message: "Please provide a summary to describe the Project or Application?",
+    validate: validateInput,
   }, {
-    type: "checkbox",
-    name: "Headings2",
-    message: "What other optional headings would you like to include?",
-    choices: ["HOW TO USE", "CODE SUMMUARY EXAMPLES", "HOW TO CONTRIBUTE"]
+    type: "input",
+    name: "installation",
+    message: "Please provide instructions on how to install the software?",
+    validate: validateInput,
   }, {
-    type: "checkbox",
-    name: "techUsed",
-    message: "What technology or Frameworks did you use?",
-    choices: ["HTML", "CSS", "Javascript", "Bootstrap", "NodeJS"]
+    type: "input",
+    name: "usage",
+    message: "Please provide instructions or tips on how to use the Software?",
+    validate: validateInput,
+  }, {
+    type: "input",
+    name: "tests",
+    message: "Tests",
+    validate: validateInput,
   }, {
     type: "list",
     name: "license",
     message: "What open source licences do you require?",
-    choices: ["MIT", "GNU", "BSD Simplified","Apache 2.0", "Mozilla Public 2,0", ]
-  }
-
-  ]);
-  }
+    choices: ["MIT", "GNU Public License (GPL)", "BSD 3-Clause","Apache 2.0", "Mozilla Public 2,0", "IBM Public License" ],
+    validate: validateInput,
+  }, {
+  type: "input",
+  name: "contributing",
+  message: "Please provide the name of contributors",
+  validate: validateInput,
+  }, {
+  type: "input",
+  name: "github",
+  message: "Please provide your GitHub username",
+  validate: validateInput,
+  }, {
+    type: "input",
+    name: "email",
+    message: "Please provide your email",
+    validate: validateInput,
+  },
+])
+}
 // TODO: Create a function to write README file
 // function writeToFile(answers) {
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,50 +1,78 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicense(data) {
+  if(data === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (data === "GNU Public License (GPL)") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (data === "BSD 3-Clause") {
+    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  } else if (data === "Apache 2.0") {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  } else if (data === "Mozilla Public 2.0") {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  } else if (data === "Eclipse Public License 1.0") {
+    return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+  } else if (data = "") {
+    return ""
+  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if(license === "MIT") {
-    let licenseInput = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+
+function validateInput(data) {
+  if(data != "") {
+    return true;
+  } else {
+    return "Please enter a response to the query"
   }
 }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data,licenseInput) {
-  return `# ${data.title}
-  ## INTRODUCTION
+function generateMarkdown(data) {
+  return `
+  
+  # ${data.title}
 
-  #### Techonologies Used in the Development of the Application
-    * ${data.techUsed}
+  ${renderLicense(licenseInput)}
 
-  ## ${data.Headings1} 
+  ## TABLE OF CONTENTS
+  * [Description](#description)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Tests](#Tests)
+  * [License](#License)
+  * [Contributors](#Contributors)
+  * [Credits](#Credits)
+  * [Questions](#Questions)
 
-  ## MOTIVATION
+  ## DESCRIPTION
+  ${data.desciption}
+  
+  ## INSTALLATION
+  ${data.installation}
 
-  ## FUTURE DEVELOPMENTS
+  #### USAGE
+  ${data.usage}
+
+  ## TESTS
   * 
   *
   * 
+  
+  ## LICENSE
+  ${data.license}
 
-  ## SKILLS AND KNOWLEDGE LEARNT
-
-  ## FEATURES
-  *
-  *
-  * 
-
-  ## ${data.Headings2}
   ## CONTRIBUTORS
-  ## ACKNOWLEDGEMENTS
+  * ${data.contributors}
+
   ## CREDITS
-  ## Licence
-    ${data.license}
-    ${renderLicenseSection(licenseInput)}
+  * 
+  *
+
+  ## QUESTIONS
+  _For any questions on this Project please contact me via the my Github or email below._
+  ${data.github}
+  ${data.email}
 `;
 }
 
